a9d5d29d1a1835f9b2edb15f8fbdbd0ff54c6ddf
harisantoso <harysanttos@gmail.com>
2024-09-03 19:13:17 +0700

    web portal citeureup dgn laravel 11

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>About</title>
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+  <a href="/">Home</a>
+  <a href="/blog">Blog</a>
+  <a href="/#">About</a>
+  <a href="/contact">Contact</a>
+  <h1>About</h1>
+  <h3>Owner: {{$nama}}</h3>
+
+  <img src="img/logo-hs.png" alt="logo-hs" width="150">
+</body>
+
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+</head>
+
+<body>
+  <a href="/">Home</a>
+  <a href="/blog">Blog</a>
+  <a href="/about">About</a>
+  <a href="/#">Contact</a>
+
+  <h2>Contact</h2>
+  <ul>
+    <li><a href="https://instagram.com/harisantoso">Instagram</a></li>
+    <li> <a href="https://instagram.com/harisantoso">X</a></li>
+  </ul>
+</body>
+
+</html>
