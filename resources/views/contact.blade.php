a673129e300216cda849b7fcff4687e02c950e60
harisantoso <harysanttos@gmail.com>
2024-09-10 21:30:01 +0700

    view data

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact</title>
-</head>
-
-<body>
-  <a href="/">Home</a>
-  <a href="/blog">Blog</a>
-  <a href="/about">About</a>
-  <a href="/#">Contact</a>
-
-  <h2>Contact</h2>
-  <ul>
-    <li><a href="https://instagram.com/harisantoso">Instagram</a></li>
-    <li> <a href="https://instagram.com/harisantoso">X</a></li>
-  </ul>
-</body>
-
-</html>
+<x-layout>
+  <x-slot:title> {{$title}} </x-slot:title>
+  <h3 class="text-xl">Contact</h3>
+</x-layout>
